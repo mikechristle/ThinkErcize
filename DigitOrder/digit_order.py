e3ad39d9b2238fbf74bb19df094f1421b1471855
mikechristle <mike@christle.us>
2022-12-28 09:14:51 -0500

    Optimize event loop.

--- DigitOrder/digit_order.py
+++ DigitOrder/digit_order.py
@@ -21,20 +21,20 @@ while True:
 
     # Get all pygame events
     for event in pygame.event.get():
-        match event.type:
+        match [event.type, st.state]:
 
             # Exit if window is closed
-            case pygame.QUIT:
+            case [pygame.QUIT, _]:
                 sys.exit()
 
             # Any click starts the game
-            case pygame.MOUSEBUTTONDOWN if st.state == st.ST_IDLE:
+            case [pygame.MOUSEBUTTONDOWN, st.ST_IDLE]:
                 start_game()
                 st.state = st.ST_NEXT
                 paint()
 
             # Delay before starting next round
-            case pygame.USEREVENT if st.state == st.ST_NEXT:
+            case [pygame.USEREVENT, st.ST_NEXT]:
                 if st.cycle == 10:
                     st.state = st.ST_IDLE
                     paint()
@@ -44,7 +44,7 @@ while True:
                     delay_count = 50
 
             # Timer event generated delays
-            case pygame.USEREVENT if st.state == st.ST_SHOW:
+            case [pygame.USEREVENT, st.ST_SHOW]:
                 delay_count -= 1
                 match delay_count:
                     case 42:
@@ -62,7 +62,7 @@ while True:
                         paint()
 
             # Check each player click
-            case pygame.MOUSEBUTTONDOWN if st.state == st.ST_WAIT:
+            case [pygame.MOUSEBUTTONDOWN, st.ST_WAIT]:
                 x, y = get_xy(event.pos)
                 click(x, y)
                 paint()
